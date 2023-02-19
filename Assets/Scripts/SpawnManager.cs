b6fd79b297d99c4705a1a36407f06b938e69904a
hmuaz <muazsenol1@gmail.com>
2023-02-19 13:46:03 +0300

    SpawnManager V3
    Spawn Locations fixed

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    [SerializeField] private Transform spawnPoint1;
-    [SerializeField] private Transform spawnPoint2;
-    [SerializeField] private Transform spawnPoint3;
-    [SerializeField] private Transform spawnPoint4;
+    [SerializeField] private Transform spawnPoint11;
+    [SerializeField] private Transform spawnPoint21;
+    [SerializeField] private Transform spawnPoint31;
+    [SerializeField] private Transform spawnPoint41;
+    [SerializeField] private Transform spawnPoint12;
+    [SerializeField] private Transform spawnPoint22;
+    [SerializeField] private Transform spawnPoint32;
+    [SerializeField] private Transform spawnPoint42;
 
     [SerializeField] private GameObject goblin;
     [SerializeField] private GameObject flyingEye;
@@ -16,11 +20,18 @@ public class SpawnManager : MonoBehaviour
     [SerializeField] private GameObject mushroom;
 
     [SerializeField] private List<GameObject> enemies = new List<GameObject>();
-    [SerializeField] private List<Transform> spawnPoints = new List<Transform>();
+
+    [SerializeField] private List<Transform> line1 = new List<Transform>();
+    [SerializeField] private List<Transform> line2 = new List<Transform>();
+    [SerializeField] private List<Transform> line3 = new List<Transform>();
+    [SerializeField] private List<Transform> line4 = new List<Transform>();
+
 
     private void Start()
     {
         SpawnEnemy();
+        //Instantiate(goblin, spawnPoint21.position, Quaternion.identity);
+
     }
     private void SpawnEnemy()
     {
@@ -44,16 +55,60 @@ public class SpawnManager : MonoBehaviour
 
     private Transform RandomSpawnPointGenerator(GameObject enemy)
     {
-        int listSize = spawnPoints.Count;
-        int randomIndex = UnityEngine.Random.Range(0, listSize);
-        enemy.GetComponent<EnemyStateManager>().enemyLineID = randomIndex + 1;
-        enemy.GetComponentInChildren<SpriteRenderer>().sortingLayerName = (randomIndex + 1).ToString();
+        int randomLineID = UnityEngine.Random.Range(1, 5);
+        if(randomLineID == 1)
+        {
+            int lineListSize = line1.Count;
+            int randomSpawnPointID = UnityEngine.Random.Range(0, lineListSize);
+            enemy.GetComponent<EnemyStateManager>().enemyLineID = randomLineID;
+            enemy.GetComponentInChildren<SpriteRenderer>().sortingLayerName = randomLineID.ToString();
+            Transform transform = line1[randomSpawnPointID];
+            Debug.Log("spawnPoint name: " + transform.name);
 
+            return transform;
 
-        Debug.Log("transform listSize " + listSize);
-        Debug.Log("transform randomIndex " + randomIndex);
-        Debug.Log("spawnpoint name: " + spawnPoints[randomIndex].name);
-        Transform transform = spawnPoints[randomIndex + 1];
-        return transform;
+        }
+        else if (randomLineID == 2)
+        {
+            int lineListSize = line2.Count;
+            int randomSpawnPointID = UnityEngine.Random.Range(0, lineListSize);
+            enemy.GetComponent<EnemyStateManager>().enemyLineID = randomLineID;
+            enemy.GetComponentInChildren<SpriteRenderer>().sortingLayerName = randomLineID.ToString();
+
+            Transform transform = line2[randomSpawnPointID];
+            Debug.Log("spawnPoint name: " + transform.name);
+
+            return transform;
+
+        }
+        else if (randomLineID == 3)
+        {
+            int lineListSize = line3.Count;
+            int randomSpawnPointID = UnityEngine.Random.Range(0, lineListSize);
+            enemy.GetComponent<EnemyStateManager>().enemyLineID = randomLineID;
+            enemy.GetComponentInChildren<SpriteRenderer>().sortingLayerName = randomLineID.ToString();
+
+            Transform transform = line3[randomSpawnPointID];
+            Debug.Log("spawnPoint name: " + transform.name);
+
+            return transform;
+
+        }
+        else if (randomLineID == 4)
+        {
+            int lineListSize = line4.Count;
+            int randomSpawnPointID = UnityEngine.Random.Range(0, lineListSize);
+            enemy.GetComponent<EnemyStateManager>().enemyLineID = randomLineID;
+            enemy.GetComponentInChildren<SpriteRenderer>().sortingLayerName = randomLineID.ToString();
+
+            Transform transform = line4[randomSpawnPointID];
+            Debug.Log("spawnPoint name: " + transform.name);
+
+            return transform;
+        }
+        Debug.LogError("spawnpoint is null");
+        return null;
     }
+
+    
 }
